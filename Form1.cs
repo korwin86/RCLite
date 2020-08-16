11d86b14124d3b792d9b2972702320ec0c343940
Александр Семенов <korwin86@gmail.com>
2020-08-16 22:16:03 +0700

    пробую проверки на форме

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace RCLite
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            delZeroEnd(sender, e);
             var data = new RCLData(Convert.ToDouble(textBox1.Text),
                                    Convert.ToDouble(textBox2.Text),
                                    Convert.ToDouble(textBox3.Text),
@@ -28,7 +29,7 @@ namespace RCLite
             {
                 toolTip1.SetToolTip(button1, "Количество пороха в банке должно быть больше 0");
             }
-
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,8 +38,11 @@ namespace RCLite
             foreach (TextBox tb in Controls.Cast<Control>().Where(x => x is TextBox).Select(x => x as TextBox))
             {
                 tb.Text = "0";
+                tb.KeyPress += checkInput;
+                tb.Click += emptyText;
+                tb.MouseLeave += delZero;
             }
-
+            button1.MouseEnter += delZeroEnd;
             label9.Text = "0";
         }
 
@@ -46,5 +50,54 @@ namespace RCLite
         {
             toolTip1.SetToolTip(button1, "");
         }
+
+        private void checkInput(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 127)
+            {
+                e.Handled = true;
+                toolTip1.SetToolTip(sender as Control, "Введите цифры или один разделитель " + ",");
+            }
+            else if (!Char.IsDigit(number) && number != 8 && number != 127 && (sender as Control).Text.IndexOf(',') != -1)
+            {
+                e.Handled = true;
+                toolTip1.SetToolTip(sender as Control, "Введите цифры или один разделитель " + ",");
+            }
+            else
+            {
+                toolTip1.SetToolTip(sender as Control, "");
+            }
+
+        }
+
+        private void emptyText(object sender, EventArgs e)
+        {
+            if ((sender as Control).Text == "0")
+                (sender as Control).Text = "";
+        }
+        private void delZero(object sender, EventArgs e)
+        {
+            (sender as Control).Text = (sender as Control).Text.TrimStart(new Char[] { '0' });
+
+            if ((sender as Control).Text.StartsWith(","))
+                (sender as Control).Text = "0" + (sender as Control).Text;
+            if ((sender as Control).Text == "")
+                (sender as Control).Text = "0";
+            (sender as TextBox).SelectionStart = (sender as Control).Text.Length;
+        }
+        private void delZeroEnd(object sender, EventArgs e)
+        {
+            foreach (TextBox tb in Controls.Cast<Control>().Where(x => x is TextBox).Select(x => x as TextBox))
+            {
+                if (!(tb.Text == "0") && tb.Text.Contains(","))
+                    tb.Text = tb.Text.TrimEnd(new Char[] { '0' });
+                if (tb.Text.EndsWith(","))
+                    tb.Text = tb.Text.TrimEnd(new Char[] { ',' });
+                tb.SelectionStart = tb.Text.Length;
+            }
+        }
+
+
     }
 }
